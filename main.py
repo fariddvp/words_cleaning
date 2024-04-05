4f548a5e5d474c25c8f49bbaf8d4aba85c73bcad
Farid <fariddivanpour@gmail.com>
2024-04-05 20:03:25 +0330

    Phase 03: Removed non-meaningful Words from dataframe and Correct Words.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# from src.create_token_words import create_token_words
-from src.english_words_cleaning import remove_repeated_characters
+from src.create_token_words import create_token_words
+from src.english_words_cleaning import reduce_repeated_characters
+from src.english_words_cleaning import remove_non_meaningful_words
+from src.english_words_cleaning import correct_words, stemmed_words, lemmatized_words
+from src.english_words_cleaning import unique_words, report_export_df
 import pandas as pd
 import time
 
@@ -8,24 +11,58 @@ import time
 def main():
 
     # Create token words from raw text as csv file
-    create_token_words()
+    # create_token_words()
+
 
 
     time.sleep(10)
 
-
-    
-    ### Remove repeated characters from dataframe
-    remove_repeated_characters()
     
 
-    
+    # Read data
+    df = pd.read_csv('/home/farid/Documents/words_cleaning/src/english_tokens.csv', header=None)
+    df.rename(columns={0: 'Words'}, inplace=True)
 
     
+    ### 01--Remove repeated characters from dataframe
+    df = reduce_repeated_characters(df)
+    print(df.head(20))
 
 
+    
+    ### 02--Remove non-meaningful words
+    df = remove_non_meaningful_words(df)
+    print(df.head(20))
+
+    
+    ### 03--Correcting words in dataset
+    df = correct_words(df)
+    print(df.head(20))
+
+    
+    # Stemming words
+    df = stemmed_words(df)
+    print(df.head(20))
+
+    
+    # Lemmatize words
+    df = lemmatized_words(df)
+    print(df.head(20))
+
+    
+    # Correct again words
+    df = correct_words(df)
+    print(df.head(20))
+
+    
+    # Unique words
+    df = unique_words(df)
+    print(df.head(20))
 
 
+    # Reporting and export
+    df = report_export_df(df)
+    print(df.head(20))
 
 
 
--- src/english_words_cleaning.py
+++ src/english_words_cleaning.py
@@ -7,41 +7,118 @@ from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
 ##### Remove repeated characters from dataset.
-def remove_repeated_characters():
+# Class for reductions iterative letters
+class RepeatReplacer():
+    def __init__(self):
+        self.repeat_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
+        self.repl = r'\1\2\3'
 
-    # Read data
-    df = pd.read_csv('/home/farid/Documents/words_cleaning/src/english_tokens.csv', header=None)
-    df.rename(columns={0: 'Words'}, inplace=True)
-    
-    # Class for reductions iterative letters
-    class RepeatReplacer():
-        def __init__(self):
-            self.repeat_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
-            self.repl = r'\1\2\3'
+    def replace(self, word):
+        if wordnet.synsets(word):
+            return word
+        repl_word = self.repeat_regexp.sub(self.repl, word)
+        if repl_word != word:
+            return self.replace(repl_word)
+        else:
+            return repl_word
 
-        def replace(self, word):
-            if wordnet.synsets(word):
-                return word
-            repl_word = self.repeat_regexp.sub(self.repl, word)
-            if repl_word != word:
-                return self.replace(repl_word)
-            else:
-                return repl_word
-
-    def reduce_repeated_characters(df):
-        replacer = RepeatReplacer()
-        replaced_words = []
-        for word in df['Words']:
-            replaced_word = replacer.replace(word)
-            replaced_words.append(replaced_word)
-            print(f"{word.ljust(20)}: Replaced with ---> '{replaced_word}'")
-        df['Reduction Words'] = replaced_words
-        return df
-
-
-    # Reduce repeated characters
-    df = reduce_repeated_characters(df)
-    print(df.head(20))
+# Remove repeated characters from dataset.
+def reduce_repeated_characters(df):
+    replacer = RepeatReplacer()
+    replaced_words = []
+    for word in df['Words']:
+        replaced_word = replacer.replace(word)
+        replaced_words.append(replaced_word)
+        print(f"{word.ljust(20)}: Replaced with ---> '{replaced_word}'")
+    df['Reduction Words'] = replaced_words
 
     print('Step Remove_Repeated_Characters completed ---> Reductioned repeated characters from dataset.')
+    return df
+
     
+
+##### Delete non-information words from dataset.
+#Function for remove non-meaningful words from dataset.
+def remove_non_meaningful_words(df):
+    df['Meaning Words'] = df['Reduction Words'].apply(lambda x: '' if not wordnet.synsets(x) else x)
+#     df = df[df['Meaning Words'] != '']  # Remove rows with empty words
+    
+    print('Step Delete_Non_Meaning_words completed ---> Remove non-meaningful words from dataset.')
+    return df
+
+
+
+
+
+##### Correcting words in dataset.
+# Initialize spell checker
+def correct_words(df):
+
+    spell = Speller()
+
+    # Auto-correct words
+    df['Corrected Words'] = df['Meaning Words'].apply(spell)
+
+    print('Step Correct_Words completed ---> Corrected words in dataset.')
+    return df
+
+
+
+# Initialize Porter Stemmer
+def stemmed_words(df):
+
+    stemmer = PorterStemmer()
+
+    # Perform stemming
+    df['Stemmed Words'] = df['Corrected Words'].apply(lambda x: stemmer.stem(x))
+
+    print('Step Stemming completed ---> Stemmed words in dataset.')
+    return df
+
+
+
+
+
+# Download WordNet if not already downloaded
+def lemmatized_words(df):
+
+    nltk.download('wordnet')
+
+    # Initialize WordNet Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # Perform lemmatization
+    df['Lemmatized Words'] = df['Stemmed Words'].apply(lambda x: lemmatizer.lemmatize(x))
+
+    print('Step Lemmtization completed ---> Lemmatized words in dataset.')
+    return df
+
+
+
+# Dropping duplicated words from dataset
+def unique_words(df):
+
+    df['Unique Words'] = df['Lemmatized Words'][~df['Lemmatized Words'].duplicated(keep='first')]
+    print(df)
+    print('Step Delete_Duplicate_Words completed ---> Droped duplicated words from dataset.')
+
+    return df
+
+
+
+
+# fill new dataframe with unique words
+def report_export_df(df):
+    # Count of primary dataset
+    count_total = df['Words'].count()
+    # Count of NaN dataset (duplicated words) in 'Unique Words' column
+    count_nan = df['Unique Words'].isna().sum()
+
+    df_new = pd.DataFrame(df[~df['Unique Words'].isna()].loc[:,'Unique Words'])
+    # Update indexes new dataframe
+    df_new.reset_index(drop=True, inplace=True)
+    # Export dataset to csv
+    df_new.to_csv('output_Unique_Words_without_NaN.csv', index=False)
+    print('Step final completed ---> Exported final csv file from dataset.')
+    print(f'Count of first dataset is : {count_total} and count of duplicated words are : {count_nan} So final export csv conisist of : {count_total - count_nan} entities.')
+    return df_new
